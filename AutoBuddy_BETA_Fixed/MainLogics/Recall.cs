019811ad25595743ece3eccb8ea70ebb47157a9a
EnfermeraSexy <santiago_97@live.com>
2016-02-03 00:55:41 +0100

    Fixed when champions walks around nexus

--- AutoBuddy_BETA_Fixed/MainLogics/Recall.cs
+++ AutoBuddy_BETA_Fixed/MainLogics/Recall.cs
@@ -142,6 +142,7 @@ AutoBuddy won't recall if you have less gold than needed for next item.
         private void CastRecall()
         {
             if (ObjectManager.Player.Distance(spawn) < 500) return;
+            lastRecallTime = Game.Time + 2f;            
             ObjectManager.Player.Spellbook.CastSpell(SpellSlot.Recall);           
         }
     }
